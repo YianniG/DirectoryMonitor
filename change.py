4f058f4c5fc9ffdb8ba4976df07479aa564c303a
Ioannis Gabrielides <ioannisgabrielides@gmail.com>
2019-10-20 21:01:56 +0100

    fix bugs

--- change.py
+++ change.py
@@ -19,18 +19,18 @@ def check_for_updates(repo_path, name):
         # Update
         completed_process = subprocess.call(["/usr/bin/git", "pull"], cwd=repo_path);
         install(repo_path, name)
-        print("updating " + )
+        print("updating " + name)
 
 def install(repo_path):
-    subprocess.call([repo_path + "install.sh"])
+    subprocess.call([repo_path + "/install.sh"])
     any_updates = True
 
 # read tracking file
 track = open("/usr/local/etc/track.csv", "r")
-for line in f:
+for line in track:
     tracking_data = line.split(',')
     name = tracking_data[0]
-    gh_url = tracking_data[1]
+    gh_url = tracking_data[1].strip()
     process_repo(gh_url, name)
 
 track.close()
